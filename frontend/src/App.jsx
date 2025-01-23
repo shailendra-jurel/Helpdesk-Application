41919ed5f2d2cceab9df0c9451787cf94d38e26d
Shailendra Jurel <shailendrajurel001@gmail.com>
2025-01-23 19:39:33 +0530

    first commit  , initialized  backend and frontendt

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1> this  is  just an Assignment  I will do it  in 2 hours only</h1>
+    </>
+ 
+  )
+}
+
+export default App
